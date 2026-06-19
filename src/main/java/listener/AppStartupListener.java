38a97c9c227427ad900b482bbf307a5c32bcc430
Vinod <myninthid2004@gmail.com>
2026-06-19 23:00:40 +0530

    simplify startup listener

--- src/main/java/listener/AppStartupListener.java
+++ src/main/java/listener/AppStartupListener.java
@@ -1,14 +1,8 @@
 package listener;
 
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.util.Enumeration;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-
-import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 
 import util.DatabaseInitializer;
 
@@ -28,39 +22,18 @@ public class AppStartupListener implements ServletContextListener {
 
 		} catch (Exception e) {
 
-			System.out.println("DATABASE INIT FAILED - APP STILL STARTING");
+			System.out.println("DATABASE INIT FAILED");
 
 			e.printStackTrace();
 
 		}
+
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
 
 		System.out.println("Application Stopped");
-
-		try {
-
-			AbandonedConnectionCleanupThread.checkedShutdown();
-
-			Enumeration<Driver> drivers = DriverManager.getDrivers();
-
-			while (drivers.hasMoreElements()) {
-
-				Driver driver = drivers.nextElement();
-
-				DriverManager.deregisterDriver(driver);
-
-			}
-
-			System.out.println("MySQL cleanup completed");
-
-		} catch (Exception e) {
-
-			e.printStackTrace();
-
-		}
 
 	}
 
